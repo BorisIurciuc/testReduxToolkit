3a33675c16a7def24b3475fb50a933f94dd97781
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-05-15 01:01:11 +0200

    add productsSlice.ts

---
+++ src/features/products/ProductsSlice.ts
@@ -0,0 +1,28 @@
+import ProductsState from './types/ProductsState'
+import * as api from './api'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import reducer from '../users/UserSlice'
+
+const initialState: ProductsState = {
+	products: [],
+}
+
+export const loadProducts = createAsyncThunk('products/loadProducts', () => api.fetchProducts())
+export const deleteProduct = createAsyncThunk('products/deleteProduct', (id: number) =>
+	api.deleteProduct(id)
+)
+export const productsSlice = createSlice({
+	name: 'products',
+	initialState,
+	reducers: {},
+	extraReducers: builder => {
+		builder
+			.addCase(loadProducts.fulfilled, (state, action) => {
+				state.products = action.payload
+			})
+			.addCase(deleteProduct.fulfilled, (state, action) => {
+				state.products = state.products.filter(product => product.id !== action.payload.id)
+			})
+	},
+})
+export default productsSlice.reducer
