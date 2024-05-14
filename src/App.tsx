3ca7bb540bc6b0eeaece7e126bb8f0130a39e364
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-05-14 23:35:36 +0200

    delete component products

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Counter } from './features/counter/Counter'
 import { Quotes } from './features/quotes/Quotes'
 import Layout from './layouts/Layout'
 import Home from './components/Home/Home'
-import Products from './features/products/Products'
 import Users from './features/users/Users'
 
 function App(): JSX.Element {
@@ -16,7 +15,6 @@ function App(): JSX.Element {
 					<Route index element={<Home />} />
 					<Route path="counter" element={<Counter />} />
 					<Route path="quotes" element={<Quotes />} />
-					<Route path="products" element={<Products />} />
 					<Route path="users" element={<Users />} />
 				</Route>
 			</Routes>
--- src/features/products/selectors.ts
+++
@@ -1,4 +0,0 @@
-import Product from './types/Product'
-import { RootState } from '../../app/store'
-
-export const selectProducts = (state: RootState): Product[] => state.products.products
