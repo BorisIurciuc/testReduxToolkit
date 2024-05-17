3ab8ebd769f8e1d7ada705433c5aec6afceca601
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-05-18 01:06:28 +0200

    apduate chatGPT

--- src/features/products/ProductEdit.tsx
+++ src/features/products/ProductEdit.tsx
@@ -1,31 +1,19 @@
-import { FormEvent, useState } from 'react'
-import { useAppDispatch } from '../../app/hooks'
-import { editTitle } from './ProductsSlice'
+interface ProductEditProps {
+	title: string
+	setTitle: (newTitle: string) => void
+}
 
-export default function ProductEdit(props: { idProd: number }): JSX.Element {
-	const { idProd } = props
-	const [title, setTitle] = useState<string>('')
-	const dispatch = useAppDispatch()
-
-	function handleSubmit(e: FormEvent<HTMLFormElement>): void {
-		e.preventDefault()
-		dispatch(editTitle({ title, id: idProd }))
-		setTitle('')
-	}
-	console.log('idProd', idProd)
-	console.log('title ', title)
+const ProductEdit: React.FC<ProductEditProps> = ({ title, setTitle }) => {
 	return (
 		<div>
-			<form onSubmit={handleSubmit}>
-				<input
-					type="text"
-					placeholder="new title"
-					value={title}
-					onChange={e => setTitle(e.target.value)}
-				/>
-				{title}
-				<button type="submit">ok</button>
-			</form>
+			<input
+				type="text"
+				placeholder="new title"
+				value={title}
+				onChange={e => setTitle(e.target.value)}
+			/>
 		</div>
 	)
 }
+
+export default ProductEdit
