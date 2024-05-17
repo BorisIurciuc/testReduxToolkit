af292d2650759a824445f04c6706d7f8ba8b6a2e
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-05-18 01:07:43 +0200

    Revert "little update"
    This reverts commit d9769014471e57413512113b0482782b315ca12c.

--- src/features/products/ProductEdit.tsx
+++ src/features/products/ProductEdit.tsx
@@ -1,9 +1,18 @@
-interface ProductEditProps {
-	title: string
-	setTitle: (newTitle: string) => void
-}
+import { FormEvent, useState } from 'react'
+import { useAppDispatch } from '../../app/hooks'
+import { editTitle } from './ProductsSlice'
 
-const ProductEdit: React.FC<ProductEditProps> = ({ title, setTitle }) => {
+export default function ProductEdit(props: { idProd: number }): JSX.Element {
+	const { idProd } = props
+	const [title, setTitle] = useState<string>('')
+	const dispatch = useAppDispatch()
+
+	function handleSubmit(e: FormEvent<HTMLFormElement>): void {
+		e.preventDefault()
+		dispatch(editTitle({ title, id: idProd }))
+	}
+	console.log('idProd', idProd)
+	console.log('title ', title)
 	return (
 		<div>
 			<input
