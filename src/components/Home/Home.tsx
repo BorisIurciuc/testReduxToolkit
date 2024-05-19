b0e821e8ae380cc8e8100217a0926d159ad2b6eb
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-05-19 17:20:06 +0200

    edit component Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,20 @@
+import style from './Home.module.css'
+
 export default function Home(): JSX.Element {
 	return (
-		<div>
+		<div className={style.containerHome}>
 			<h1>Home</h1>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. In inventore amet minus nobis
+				<br />
+				ipsam expedita reprehenderit repellat voluptatibus minima, quaerat facere voluptates! Soluta
+				<br />
+				laboriosam iste corrupti alias explicabo ut atque.
+			</p>
+			<img
+				src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2017_04/1876141/170123-pluto-new-horizons-mn-1400.jpg"
+				alt=""
+			/>
 		</div>
 	)
 }
